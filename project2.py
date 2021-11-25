25046766c696e5eaea7cf886e37053562e6dba9d
MounaDH <mounaadhaouadi@gmail.com>
2021-11-25 11:01:50 -0500

    train data analysis

--- project2.py
+++ project2.py
@@ -1,18 +1,46 @@
 from os import listdir
 from tqdm import tqdm
-import plotly.express as px
 import pandas as pd
 import numpy as np
-import plotly.graph_objects as go
-import json
+import matplotlib.pyplot as plt
+
 
 def dev_data_analysis(folder):
     pass
 
 
-def make_image_distribution(distribution):
-    pass
+def make_image_distribution(df):
+    # print(list(df['length']))
+    # plt.hist(list(df['length']), bins=11)
+    # plt.show()
 
+    print('creating image .. ')
+    a = len(df[df['length'] < 5])
+    b = len(df[(df['length'] >= 5) & (df['length'] < 10)])
+    c = len(df[(df['length'] >= 10) & (df['length'] < 15)])
+    d = len(df[(df['length'] >= 15) & (df['length'] < 20)])
+    e = len(df[(df['length'] >= 20) & (df['length'] < 25)])
+    f = len(df[(df['length'] >= 25) & (df['length'] < 30)])
+    g = len(df[(df['length'] >= 30) & (df['length'] < 35)])
+    h = len(df[(df['length'] >= 35) & (df['length'] < 40)])
+    i = len(df[(df['length'] >= 40) & (df['length'] < 45)])
+    j = len(df[(df['length'] >= 45) & (df['length'] < 50)])
+    k = len(df[df['length'] >= 50])
+
+    lengthes_per_cat = [a, b, c, d, e, f, g, h, i, j, k]
+    categories = ["<5", "5-10", "10-15", "15-20", "20-25", "25-30", "30-35", "35-40", "40-45", "45-50", ">50"]
+
+    fig = plt.figure(figsize=(7, 4))
+    plt.bar(categories, lengthes_per_cat, color='blue', width=1)
+
+    plt.xlabel("Sentences lengths (number of words)")
+    plt.ylabel("Number of sentences")
+    #plt.title("Distribution of the train sentences lengths")
+    plt.show()
+
+    fig.savefig('plots/length_distribution_train.png')
+    fig.savefig('plots/length_distribution_train.eps')
+    fig.savefig('plots/length_distribution_train.pdf')
 
 
 def train_data_analysis(folder):
@@ -30,21 +58,26 @@ def train_data_analysis(folder):
             lengthes.extend( [len(sent.split()) for sent in sentences] )
 
     print(f'total nb of train sentences {total_nb_train_sentences}')
-    print('lengthes',lengthes )
+    #print('lengthes',lengthes )
     df = pd.DataFrame(lengthes, columns=['length'])
-    # df.to_csv('lengths_distribution.csv')
-
-    # todo: make histogramme data from df
-
-    make_image_distribution(df)
+    df.to_csv('lengths_distribution.csv')
 
 
 train_data_folder = 'train data/training-monolingual.tokenized.shuffled/'
 dev_data_folder = 'dev data/'
+news_file = dev_data_folder + 'news.ref'
+euro_file = dev_data_folder + 'euro.ref'
+hans_file = dev_data_folder + 'hans.ref'
 
 
 def main():
-    train_data_analysis(train_data_folder)
+    # train_data_analysis(train_data_folder)
+
+    df = pd.read_csv('lengths_distribution.csv')
+    df = df.drop(columns=['Unnamed: 0'])
+    make_image_distribution(df)
+
+
     #dev_data_analysis(dev_data_folder)
 
 
